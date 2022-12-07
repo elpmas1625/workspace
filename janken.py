6021a86eb9f914542b54771557ca6ff81050f3f6
elpmas1625 <elpmas.1625@gmail.com>
2022-12-07 16:25:06 +0900

    Update janken.py

--- janken.py
+++ janken.py
@@ -10,7 +10,7 @@ class Janken:
     def start(self):
         pName = input("Enter your name : ")
         # Playerクラスからインスタンスを生成してみよう．(ヒント：下のCpuクラスの場合を参考に)
-        player = Player()
+        player = Player(pName)
 
         # Cpuクラスからcpuインスタンスを生成
         cpu = Cpu("CPU")
